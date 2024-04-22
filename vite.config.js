9b094be0c270ee526eb206bd9275fddc1cb728ce
Zaks0n <zaksontech@gmail.com>
2024-04-22 22:53:08 +0100

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/second-semester-exams/github-api-portfolio/",
+  base: "/github-api-portfolio/",
   plugins: [react()],
 })
